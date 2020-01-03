38e9e40ce1ac1690d5aa87cd28af3b9cb3517faa
down_time_007 <dhrumilnvaghela@gmail.com>
2020-01-03 12:15:24 +0530

    Add files via upload

---
+++ method2.groovy
@@ -0,0 +1,13 @@
+package com.test.demo
+
+class Example { 
+ 
+   static void main(args) { 
+      Example abc = new Example(); 
+      abc.mymethod(); 
+   }
+   
+   public void mymethod() {
+	   println "this is in method";
+	}
+}
